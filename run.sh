a574c4d0f202bbc94ff25fe7e0338c8cc32b7d02
to52673 <florent.boudet@cassidian.com>
2016-03-29 17:10:48 +0200

    Ajusted Dockerfile and run script

--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# post-apply script that properly manages ssh authentication keys
+# Install in /var/radmind/postapply
+
+SSHKEYGEN=/usr/bin/ssh-keygen
+
+if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+    $SSHKEYGEN -q -t rsa  -f /etc/ssh/ssh_host_rsa_key -N "" \
+        -C "" < /dev/null > /dev/null 2> /dev/null
+    echo "Created /etc/ssh_host_rsa_key"
+fi
+
+if [ ! -f /etc/ssh/ssh_host_dsa_key ]; then
+    $SSHKEYGEN -q -t dsa  -f /etc/ssh/ssh_host_dsa_key -N "" \
+        -C "" < /dev/null > /dev/null 2> /dev/null
+    echo "Created /etc/ssh_host_dsa_key"
+fi
+
 # Install SSH keys
 mkdir -p ~/.ssh
 echo $JENKINS_PUBLIC_KEY | sed 's/\\\\n/\n/g' > ~/.ssh/authorized_keys
