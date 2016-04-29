cef73b565094b7a98170f98119526f4e58f3e856
flboudet <flboudet@users.noreply.github.com>
2016-04-29 15:40:04 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,6 +23,7 @@ echo $JENKINS_PUBLIC_KEY | sed 's/\\\\n/\n/g' > ~/.ssh/authorized_keys
 echo $GITLAB_SSH_PRIVATE_KEY | sed 's/\\\\n/\n/g' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
+echo -e "Host u-test-factory.org\n\tStrictHostKeyChecking no\n\n" >> ~/.ssh/config
 echo -e "Host gitlab.u-test-factory.org\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 # Run sshd
